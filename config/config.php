1217d3ab088ebfbbdc4b79a82a594d4cfb042f26
Sai Manudeep Gadde <manudeepgadde01@gmail.com>
2021-05-05 12:39:54 -0500

    heroku deploy check

--- config/config.php
+++ config/config.php
@@ -6,18 +6,17 @@
 
     $timezone = date_default_timezone_set("America/Chicago");//default timezone for the entire code
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
-    $cleardb_username = $cleardb_url["bd0ad42a728153"];
-    $cleardb_password = $cleardb_url["a9256163"];
-    $cleardb_db = substr($cleardb_url["heroku_a4123219fdcd06e"],1);
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
     $active_group = 'default';
     $query_builder = TRUE;
-    
     // Connect to DB
-    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
-    // $con = mysqli_connect("localhost","root","179601","social");
+    $con = mysqli_connect("localhost","root","179601","social");
     
     if(mysqli_connect_errno()){
         echo "Failed to connect:".mysqli_connect_errno();
